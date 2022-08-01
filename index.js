ff50e6edb4bdb1d2237abd64e2270d80bc22eea4
Kelly McGuffog <kellymcguffog@gmail.com>
2022-08-01 10:50:14 -0400

    Show target elements details on click

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
+
+    const specials = document.getElementById('cocktail-specials');
+    const details = document.getElementById('cocktail-details');
+
     function cocktail(drink){
-        const specials = document.getElementById('cocktail-specials');
         const card = document.createElement('div')
         card.className = "card"
         specials.append(card)
@@ -13,17 +16,15 @@ document.addEventListener("DOMContentLoaded", () => {
         const cocktailDescription = document.createElement('p')
         cocktailDescription.className = "cocktailDescription"
         cocktailDescription.textContent = drink.description
+        const ingredients = document.createElement("p")
+        ingredients.className = "ingredients"
+        ingredients.innerText = drink.ingredients
+        ingredients.style.display = "none"
         const cardBtn = document.createElement('button')
         cardBtn.className = "cardBtn"
         cardBtn.innerText = "See More"
-        card.append(cocktailImg, cocktailName, cocktailDescription, cardBtn)
-        cardBtn.addEventListener("click", (e) =>{
-            specials.innerHTML = ""
-            console.log(e.target.parentNode)
-            const list = document.getElementById('cocktail-list')
-            const targetCard = e.target.parentNode;
-            list.append(targetCard)
-        })
+        card.append(cocktailImg, cocktailName, cocktailDescription, ingredients, cardBtn)
+        cardBtn.addEventListener("click", showDetails)
     }
     function renderCocktails(){
         const showCocktails = document.getElementById("cocktails")
@@ -32,6 +33,26 @@ document.addEventListener("DOMContentLoaded", () => {
         .then(res => res.json())
         .then(data => data.map(drink => cocktail(drink)))
     })
+    }
+
+    function showDetails(e){
+        specials.innerHTML = ""
+        const targetImgDiv = document.getElementById('cocktail-target-img')
+        const targetImgSrc = e.target.parentNode.getElementsByClassName("cocktailImg")[0].currentSrc
+        const targetImg = document.createElement("img")
+        targetImg.src = targetImgSrc
+        targetImgDiv.append(targetImg)
+        const divDetails = document.createElement('div')
+        const cocktailname = document.createElement('h3')
+        cocktailname.textContent = e.target.parentNode.getElementsByClassName("cocktail-name")[0].innerHTML
+        details.append(divDetails)
+        const targetCocktailDescription = document.createElement('p')
+        targetCocktailDescription.textContent = e.target.parentNode.getElementsByClassName("cocktailDescription")[0].innerHTML
+        const targetIngredients = document.createElement('p')
+        targetIngredients.textContent = e.target.parentNode.getElementsByClassName("ingredients")[0].innerHTML
+        targetIngredients.style.display = "block"
+        divDetails.append(cocktailname, targetCocktailDescription, targetIngredients)
+        console.log(e.target.parentNode.getElementsByClassName("ingredients"))
     }
 
     renderCocktails()
