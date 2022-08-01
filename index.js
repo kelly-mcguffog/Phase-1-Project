552bfd0985888c35a4b651a07641eb8956382f48
Kelly McGuffog <kellymcguffog@gmail.com>
2022-07-31 23:33:23 -0400

    Hide elements when target element is clicked

--- index.js
+++ index.js
@@ -17,11 +17,17 @@ document.addEventListener("DOMContentLoaded", () => {
         cardBtn.className = "cardBtn"
         cardBtn.innerText = "See More"
         card.append(cocktailImg, cocktailName, cocktailDescription, cardBtn)
+        cardBtn.addEventListener("click", (e) =>{
+            specials.innerHTML = ""
+            console.log(e.target.parentNode)
+            const list = document.getElementById('cocktail-list')
+            const targetCard = e.target.parentNode;
+            list.append(targetCard)
+        })
     }
     function renderCocktails(){
         const showCocktails = document.getElementById("cocktails")
         showCocktails.addEventListener('click', () => {
-            console.log('hi')
         fetch("http://localhost:3000/drinks")
         .then(res => res.json())
         .then(data => data.map(drink => cocktail(drink)))
