d06435c65d85657ce96adac71c3d4c9920375235
Kelly McGuffog <kellymcguffog@gmail.com>
2022-07-31 22:45:31 -0400

    Started building out cocktail card in JS

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-    
+    function cocktail(){
+        const specials = document.getElementById('cocktail-specials');
+        const card = document.createElement('div')
+        card.className = "card"
+        specials.append(card)
+        const cocktailImg = document.createElement('img')
+        cocktailImg.className = "cocktailImg"
+        cocktailImg.src= "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/marg-h-1526063646.jpg";
+        const cocktailName = document.createElement('h3')
+        cocktailName.className = "cocktail-name"
+        cocktailName.innerText = "Margarita"
+        const cocktailDescription = document.createElement('p')
+        cocktailDescription.className = "cocktailDescription"
+        cocktailDescription.textContent = "A refreshing blend of sweet and tangy"
+        const cardBtn = document.createElement('button')
+        cardBtn.className = "cardBtn"
+        cardBtn.innerText = "See More"
+        card.append(cocktailImg, cocktailName, cocktailDescription, cardBtn)
+    }
+    cocktail()
 })
